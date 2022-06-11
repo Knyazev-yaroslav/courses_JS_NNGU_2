2ec81b3b40a208f8a4f3def46c5ccdae9188c8c7
Knyazev Yaroslav <knyazev.yaroslav2020@gmail.com>
2022-06-11 17:50:10 +0300

    lesson_7

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './assets/scss/normalize.scss';
-import MainPage from './components/MainPage';
+import TestPage from './entities/TestPage';
+import UsersPage from './entities/UsersPage';
+import UserComponent from './entities/UsersPage/components/UserComponent';
+import PageLayout from './layouts/PageLayout';
 
 const App = () => {
-  return <MainPage />;
+  return (
+    <PageLayout>
+      <Routes>
+        <Route path="/" element={<UsersPage />} />
+        <Route path="/users_page" element={<UsersPage />} />
+        <Route path="users_page/:id" element={<UserComponent />} />
+        <Route path="/test_page" element={<TestPage />} />
+      </Routes>
+    </PageLayout>
+  );
 };
 
 export default App;
